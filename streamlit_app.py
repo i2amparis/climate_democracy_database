e66dfe7e02f1fd6da0183566ebc1409b4b33877a
George Xexakis <gxexakis@gmail.com>
2024-10-18 21:58:42 +0300

    first demo version

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,45 +2,53 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx", "xls"])
+st.title('Climate democracy in Europe')
+st.logo('media/logo.svg', size='large', link=None, icon_image=None)
 
-if uploaded_file is not None:
-    file_extension = uploaded_file.name.split(".")[-1].lower()
-    #Check for csv 
-    if file_extension == "csv":
-            df = pd.read_csv(uploaded_file)
-    #Check for xlsx or xls
+st.markdown('''
+    The following dataset provides information about climate democracy 
+    on all EU member states and UK since 1990. There are over 100 variables 
+    about various topics related to climate action, energy policy, and democratic practices.
+    ''')
+
+df = pd.read_excel("input_data/202409_climate_democracy_data_clean.xlsx")
+df_meta = pd.read_excel("input_data/202409_climate_democracy_metadata.xlsx",
+    sheet_name='Variables', index_col='Variable')
+
+
+# df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+country_values = st.multiselect("Select Country:", df["countryname"].unique())
+# scenario_values = st.multiselect("Select Scenarios:", df["Scenario"].unique())
+# region_values = st.multiselect("Select Regions:", df["Region"].unique())
+variable_value = st.selectbox("Select a Variable:", df["variable"].unique())
+
+if country_values and variable_value:
+    filtered_df = df[(df["countryname"].isin(country_values)) & 
+                    (df["variable"]== variable_value)]
+
+    var_desc = (df_meta.loc[variable_value,'Interpretation'])
+    var_source = (df_meta.loc[variable_value,'Source'])
+
+    st.markdown(f'**Variable description:** {var_desc}')
+    st.markdown(f'**Variable source:** {var_source}')
+
+    if not filtered_df.empty:
+        plt.figure(figsize=(10, 6))
+
+        for country in country_values:
+            subset = filtered_df[(filtered_df["countryname"] == country)]
+            if not subset.empty:
+                label = f'{country}'
+                plt.plot(subset.observation_year, subset.value, marker='o', linestyle='-', label=label)
+                    
+        plt.xlabel("Years")
+        plt.ylabel("")  
+        plt.grid(True)
+        plt.legend()
+        st.pyplot(plt)
     else:
-            df = pd.read_excel(uploaded_file)
-    #lower case
-    df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
-    country_values = st.multiselect("Select Country:", df["countryname"].unique())
-    # scenario_values = st.multiselect("Select Scenarios:", df["Scenario"].unique())
-    # region_values = st.multiselect("Select Regions:", df["Region"].unique())
-    variable_value = st.selectbox("Select a Variable:", df["variable"].unique())
+        st.warning('No data available for the selected choices.')
+    st.write(filtered_df.iloc[:,1:])
+else:
+    st.warning('Please select at least one option for each category.')
 
-    if country_values and variable_value:
-        filtered_df = df[(df["countryname"].isin(country_values)) & 
-                        (df["variable"]== variable_value)]
-
-        if not filtered_df.empty:
-            plt.figure(figsize=(10, 6))
-
-            for country in country_values:
-                subset = filtered_df[(filtered_df["countryname"] == country)]
-                if not subset.empty:
-                    label = f"{country}"
-                    plt.plot(subset.observation_year, subset.value, marker='o', linestyle='-', label=label)
-                            
-
-            plt.title(f"Line Plot for {variable_value}")
-            plt.xlabel("Years")
-            plt.ylabel("")  
-            plt.grid(True)
-            plt.legend()
-            st.pyplot(plt)
-        else:
-            st.warning('No data available for the selected choices.')
-        st.write(filtered_df)
-    else:
-        st.warning('Please select at least one option for each category.')
